5b4289173c637837badc5c0e55d5029a801b223f
koxx12-dev <80456351+koxx12-dev@users.noreply.github.com>
2021-05-12 17:12:18 +0200

    optifine fix

--- src/commands/skyclient/mod/modinfo.ts
+++ src/commands/skyclient/mod/modinfo.ts
@@ -46,8 +46,9 @@ export default class modinfo extends BotCommand {
         if (url) {
             if (url == 'https://optifine.net/download?f=OptiFine_1.8.9_HD_U_M5.jar') {
                 modinfoembed.addField(`Direct Download`, `Would be here, but I don't want to get into legal issues with SP, so go find it yourself.`)
+            } else {
+                modinfoembed.addField("Direct Download", `[Click here!](${url})`);
             }
-            modinfoembed.addField("Direct Download", `[Click here!](${url})`);
             let size = parseInt((await axios.head(url)).headers["content-length"], 10);
             if (size) {
                 modinfoembed.addField("Size", `${prettyBytes(size)}`);
